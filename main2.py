c751b147067377e303f62c0df792d791ced9fb71
ricesoo <74141787+ricesoo@users.noreply.github.com>
2020-11-11 22:34:50 +0530

    Add files via upload

---
+++ main2.py
@@ -0,0 +1,21 @@
+
+num1 = float(input("Enter first number: \n"))
+op = input("Enter operator: \n")
+num2 = float(input("Enter second number: \n"))
+
+if op == "+":
+    print(num1 + num2)
+elif op == "-":
+    print(num1 - num2)
+elif op == "/":
+    print(num1 / num2)
+elif op == "*":
+    print(num1 * num2)
+elif op == "%":
+    print(num1 % num2)
+else:
+    print("Invalid operator or number!, check again ")
+
+
+
+
